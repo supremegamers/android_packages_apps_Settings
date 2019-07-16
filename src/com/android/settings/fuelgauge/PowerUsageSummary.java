f1f52105938b97d81fcb159f80c069e2d07aa44a
spezi77 <spezi7713@gmx.net>
2019-07-16 20:47:33 +0200

    Smart Charging: Make it optional
    * Allows a device maintainer to show/hide the Smart Charging pref in Power Settings
    * Disabled by default
    
    Change-Id: I0dcbdab57e5cef2e599054f570137078a751d4ac
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -248,6 +248,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         }
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
+
+        // Check availability of Smart Charging
+        Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
+        if (!getResources().getBoolean(R.bool.config_deviceSupportSmartCharging)) {
+            getPreferenceScreen().removePreference(mSmartCharging);
+        }
     }
 
     @Override
