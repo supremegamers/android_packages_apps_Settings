supremegamers/android_packages_apps_Settings

commit c5a8c7573d35bbfb0f55529320b9c4ba2b2b3a1d

Initial Kasumi bringup
Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

src/com/android/settings/kasumi/KasumiLabs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Project Materium
+ * Copyright (C) 2021 Project Kasumi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.settings.materium;
+package com.android.settings.kasumi;
 
 import android.os.Bundle;
 
@@ -21,16 +21,16 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MateriumLabs extends SettingsPreferenceFragment {
+public class KasumiLabs extends SettingsPreferenceFragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    addPreferencesFromResource(R.xml.materium_labs);
+    addPreferencesFromResource(R.xml.kasumi_labs);
   }
 
   @Override
   public int getMetricsCategory() {
-    return MetricsProto.MetricsEvent.MATERIUM;
+    return MetricsProto.MetricsEvent.KASUMI;
   }
 }

res/drawable/ic_homepage_kasumi.xml
@@ -29,5 +29,5 @@
         android:height="@dimen/dashboard_tile_foreground_image_size"
         android:start="@dimen/dashboard_tile_foreground_image_inset"
         android:top="@dimen/dashboard_tile_foreground_image_inset"
-        android:drawable="@drawable/ic_settings_materium" />
+        android:drawable="@drawable/ic_settings_kasumi" />
 </layer-list>

res/values/custom_strings.xml
@@ -173,7 +173,7 @@
     <string name="face_redo_warning_msg">Do you want to remove the existing face scan and create a new one?</string>
 
     <!-- Settings version info -->
-    <string name="materium_version">Materium version</string>
+    <string name="kasumi_version">Kasumi version</string>
 
     <!-- Notification guts kill app button -->
     <string name="notification_guts_kill_app_button_title">Kill app button</string>
@@ -264,10 +264,10 @@
     <string name="ticker_tick_duration_title">Ticker duration</string>
     
     <!-- Labs -->
-    <string name="materium_labs">Labs</string>
-    <string name="materium_labs_desc">This is where you craft your Materium!</string>
-    <string name="materium_nshade_category_title">Notification shade</string>
-    <string name="materium_statusbar_category_title">Statusbar</string>
+    <string name="kasumi_labs">Kasumi\'s Labs</string>
+    <string name="kasumi_labs_desc">Play with Kasumi and craft your own appearance!</string>
+    <string name="kasumi_nshade_category_title">Notification shade</string>
+    <string name="kasumi_statusbar_category_title">Statusbar</string>
 
     <!-- Omni Jaws -->
     <string name="weather_title">Weather</string>

res/xml/kasumi_labs.xml
@@ -14,7 +14,7 @@
 <PreferenceScreen
   xmlns:android="http://schemas.android.com/apk/res/android"
   xmlns:settings="http://schemas.android.com/apk/res/com.android.settings"
-  android:title="@string/materium_labs">
+  android:title="@string/kasumi_labs">
 
     <!-- Gaming mode -->
     <Preference
@@ -53,8 +53,8 @@
     </Preference>   
     
     <PreferenceCategory
-          android:key="materium_statusbar_options"
-	  android:title="@string/materium_statusbar_category_title" >
+          android:key="kasumi_statusbar_options"
+	  android:title="@string/kasumi_statusbar_category_title" >
 
     <com.android.settings.widget.preferences.SystemSettingSwitchPreference
          android:key="show_fourg_icon"
@@ -85,8 +85,8 @@
     </PreferenceCategory>
     
     <PreferenceCategory
-          android:key="materium_nshade_options"
-          android:title="@string/materium_nshade_category_title" >
+          android:key="kasumi_nshade_options"
+          android:title="@string/kasumi_nshade_category_title" >
     
         <com.android.settings.widget.preferences.SystemSettingSwitchPreference
             android:key="notification_guts_kill_app_button"

res/xml/top_level_settings.xml
@@ -118,12 +118,12 @@
         settings:controller="com.android.settings.accounts.TopLevelAccountEntryPreferenceController"/>
 
     <Preference
-        android:key="top_level_materium"
-        android:title="@string/materium_labs"
-        android:summary="@string/materium_labs_desc"
-        android:icon="@drawable/ic_homepage_materium"
+        android:key="top_level_kasumi"
+        android:title="@string/kasumi_labs"
+        android:summary="@string/kasumi_labs_desc"
+        android:icon="@drawable/ic_homepage_kasumi"
         android:order="-25"
-        android:fragment="com.android.settings.materium.MateriumLabs"/>
+        android:fragment="com.android.settings.kasumi.KasumiLabs"/>
     
     <Preference
         android:key="top_level_accessibility"

commit ba4a3c814dc30624e4532754d9f5ab58ab67afef

Fixup³: Initial Kasumi bringup
* Forgot the copyright replacement. `sed` moment.

Reported-by: Nick <nift4@protonmail.com>
Fixes: c5a8c7573d35bbfb0f55529320b9c4ba2b2b3a1d
Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

src/com/android/settings/kasumi/KasumiLabs.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2021 Project Materium
  * Copyright (C) 2021 Project Kasumi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");